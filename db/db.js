5f093ff9e514e55ea46af9e51c0f0abe75d5ce45
frankmwilsonm <frank.mamani@upeu.edu.pe>
2022-07-17 01:16:07 -0500

    newdbconfig /gitu-2

--- db/db.js
+++ db/db.js
@@ -1,12 +1,20 @@
 const { Client } = require('pg');
 
 const client = new Client({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'testimageupload',
-    password: 'admin',
+    user: 'hybeeatdkhzlga',
+    host: 'ec2-3-219-52-220.compute-1.amazonaws.com',
+    database: 'd871baq3aopb9d',
+    password: '9d053184491d7ebe52ccdc19710896c1fd49db68eed3a4a3732ed72cf4791b33',
     port: 5432,
 });
+
+// const client = new Client({
+//     user: 'postgres',
+//     host: 'localhost',
+//     database: 'testimageupload',
+//     password: 'admin',
+//     port: 5432,
+// });
 
 client.connect()
     .then(console.log("Conectado a la base de Datos!"))
